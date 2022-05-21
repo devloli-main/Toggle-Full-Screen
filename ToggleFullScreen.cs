73f442b6669584de58abe720611c6f548bdeb7a4
DevloliMaybe <86964953+devloli-main@users.noreply.github.com>
2022-05-21 14:17:33 +0100

    Create ToggleFullScreen.cs

---
+++ ToggleFullScreen.cs
@@ -0,0 +1,33 @@
+using System.Windows.Forms;
+
+namespace Devloli.Modules
+{
+    internal class ToggleFullScreen
+    {
+        public static void GoFullscreen(bool fullscreen)
+        {
+            if (fullscreen)
+            {
+                Form form = new Form();
+                form.FormBorderStyle = FormBorderStyle.None;
+                form.WindowState = FormWindowState.Maximized;
+                form.Show();
+                form.Focus();
+                form.BringToFront();
+                form.TopMost = true;
+                form.Focus();
+            }
+            else
+            {
+                Form form = new Form();
+                form.FormBorderStyle = FormBorderStyle.Sizable;
+                form.WindowState = FormWindowState.Normal;
+                form.Show();
+                form.Focus();
+                form.BringToFront();
+                form.TopMost = true;
+                form.Focus();
+            }
+        }
+    }
+}
